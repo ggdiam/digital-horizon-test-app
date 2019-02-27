ea9fce93d034fa7b6cc5483a0d343a3226c89434
Alexander Sveshnikov <alexander.sveshnikov@aurea.com>
2019-02-27 09:43:04 +0300

    Updates

---
+++ precache-manifest.53f82708b1e9b61fd2413956c2578ff5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "975ae00a66b4746b8dc4",
+    "url": "/digital-horizon-test-app/static/css/main.3589fb2a.chunk.css"
+  },
+  {
+    "revision": "975ae00a66b4746b8dc4",
+    "url": "/digital-horizon-test-app/static/js/main.975ae00a.chunk.js"
+  },
+  {
+    "revision": "b8fb5870285613bbbbab",
+    "url": "/digital-horizon-test-app/static/js/runtime~main.b8fb5870.js"
+  },
+  {
+    "revision": "2d1f5f79590c147d3f0e",
+    "url": "/digital-horizon-test-app/static/js/2.2d1f5f79.chunk.js"
+  },
+  {
+    "revision": "81c0dcc8c577d3173b23c2c52b757139",
+    "url": "/digital-horizon-test-app/index.html"
+  }
+];
